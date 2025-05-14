a61a11541334967edcf5ec5e0df33b740002ae0d
AdamT <a.tupilagov@gmail.com>
2025-05-14 09:05:11 +0300

    Radio

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,59 @@
+package ru.netology;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation >= 0) {
+            if (newCurrentStation <=9) {
+                currentStation = newCurrentStation;
+            }
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume >= 0) {
+            if (newCurrentVolume <= 100) {
+                currentVolume = newCurrentVolume;
+            }
+        }
+    }
+
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
+
