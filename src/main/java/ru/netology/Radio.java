cb2862a1ced99c81a82d097a6937c525f8aac264
AdamT <a.tupilagov@gmail.com>
2025-05-19 13:39:54 +0300

    flexible

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,8 +1,17 @@
 package ru.netology;
 
 public class Radio {
-    private int currentStation;
-    private int currentVolume;
+    private int currentStation = 0;
+    private int currentVolume = 0;
+    private int numStations;
+
+    public Radio() {
+        this(10);
+    }
+
+    public Radio(int numStations) {
+        this.numStations = (numStations <= 0) ? 10 : numStations;
+    }
 
     public int getCurrentStation() {
         return currentStation;
@@ -12,39 +21,45 @@ public class Radio {
         return currentVolume;
     }
 
+    public int getNumStations() {
+        return numStations;
+    }
+
     public void setCurrentStation(int newCurrentStation) {
         if (newCurrentStation >= 0) {
-            if (newCurrentStation <=9) {
+            if (newCurrentStation < numStations) {
                 currentStation = newCurrentStation;
+            } else {
+                currentStation = 0;
             }
+        } else {
+            currentStation = 0;
         }
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume >= 0) {
-            if (newCurrentVolume <= 100) {
-                currentVolume = newCurrentVolume;
-            } else {
-                currentVolume = 100;
-            }
-        } else {
+        if (newCurrentVolume < 0) {
             currentVolume = 0;
+        } else if (newCurrentVolume > 100) {
+            currentVolume = 100;
+        } else {
+            currentVolume = newCurrentVolume;
         }
     }
 
     public void next() {
-        if (currentStation == 9) {
+        if (currentStation == numStations - 1) {
             currentStation = 0;
         } else {
-            currentStation++;
+            currentStation = currentStation + 1;
         }
     }
 
     public void prev() {
         if (currentStation == 0) {
-            currentStation = 9;
+            currentStation = numStations - 1;
         } else {
-            currentStation--;
+            currentStation = currentStation - 1;
         }
     }
 
